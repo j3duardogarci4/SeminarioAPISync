49435562697cf5651543e4ae7f08bfff0aebf7f5
juanedo77@gmail.com <juanedo77@gmail.com>
2025-08-27 18:00:01 -0400

    change format

--- SeminarioAPISync/Models/Venue.cs
+++ SeminarioAPISync/Models/Venue.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; } 
         public string Address { get; set; }
         public int SeatsAvailables { get; set; }
-
         public bool isAvailable { get; set; }
     }
 }
