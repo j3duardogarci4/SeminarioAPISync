0f06edf7b6b14677632d6836205caa2114e79e9c
juanedo77@gmail.com <juanedo77@gmail.com>
2025-08-27 17:59:27 -0400

    change properties class Venue

--- SeminarioAPISync/Models/Venue.cs
+++ SeminarioAPISync/Models/Venue.cs
@@ -3,8 +3,9 @@
     public class Venue
     {
         public int Id { get; set; } 
-        public string Adress { get; set; }
+        public string Address { get; set; }
         public int SeatsAvailables { get; set; }
 
+        public bool isAvailable { get; set; }
     }
 }
